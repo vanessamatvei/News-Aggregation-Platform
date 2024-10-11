f27f2fc032a3ea515c6540a69fce8a7237e5156a
vanessamatvei <147709263+vanessamatvei@users.noreply.github.com>
2024-10-11 19:50:06 +0300

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h2>Navigation</h2>
+            <input type="text" placeholder="Search articles..." />
+            {/* Add filter options here */}
+        </nav>
+    );
+};
+
+export default Navbar;
